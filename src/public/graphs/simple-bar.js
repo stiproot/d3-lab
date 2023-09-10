08d1b0eb4701f6ccd64b2611456dfc5218f82e1f
Simon Stipcich <simon.stipcich@derivco.co.za>
2023-09-10 10:16:24 +0200

    feat:progress on tut.

--- src/public/graphs/simple-bar.js
+++ src/public/graphs/simple-bar.js
@@ -1,6 +1,8 @@
-// import { sample_data } from "../data/nested-treemap-data.js";
+const barData = [];
+for (let i = 0; i < 50; i++) {
+  barData.push(Math.random() * 30);
+}
 
-const barData = [20, 30, 45, 15, 10, 5];
 const height = 400,
   width = 600,
   barWidth = 50,
@@ -10,6 +12,22 @@ const yScale = d3
   .scaleLinear()
   .domain([0, d3.max(barData)])
   .range([0, height]);
+
+const xScale = d3
+  .scaleBand()
+  .domain(barData)
+  .paddingInner(0.2)
+  .paddingOuter(0.1)
+  .range([0, width]);
+
+// const colors = d3
+//   .scaleLinear()
+//   .domain([0, d3.max(barData)])
+//   .range(["#FFB832", "#C61C6F"]);
+const colors = d3
+  .scaleLinear()
+  .domain([0, barData.length * 0.33, barData.length * 0.66, barData.length])
+  .range(["#FFB832", "#C61C6F", "#268BD2", "#85992C"]);
 
 export function buildSimpleBar(_data, container_id) {
   d3.select(container_id)
@@ -21,17 +39,25 @@ export function buildSimpleBar(_data, container_id) {
     .data(barData)
     .enter()
     .append("rect")
-    .style("fill", "#3c763d")
-    .attr("width", barWidth)
+    .style("fill", function (d) {
+      return colors(d);
+    })
+    .attr("width", function (d) {
+      return xScale.bandwidth();
+    })
     .attr("height", function (d) {
       return yScale(d);
     })
-    .attr("x", function (d, i) {
-      return i * (barWidth + barOffset);
+    .attr("x", function (d) {
+      return xScale(d);
     })
     .attr("y", function (d) {
       return height - yScale(d);
+    })
+    .on("mouseover", function (d) {
+      d3.select(this).style("opacity", 0.5);
+    })
+    .on("mouseout", function (d) {
+      d3.select(this).style("opacity", 1);
     });
-
-  //container.append(svg.node());
 }
