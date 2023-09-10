f04d0266dbf89e9b4d018c53d664e81c6331db57
Simon Stipcich <simon.stipcich@derivco.co.za>
2023-09-10 12:00:00 +0200

    feat:playing with margins.

--- src/public/graphs/simple-bar.js
+++ src/public/graphs/simple-bar.js
@@ -1,10 +1,12 @@
 const barData = [];
+
 for (let i = 0; i < 50; i++) {
-  barData.push(Math.random() * 30);
+  barData.push(Math.random() * 300);
 }
 
-const height = 400,
-  width = 600,
+const margin = { top: 40, right: 0, bottom: 0, left: 30 },
+  height = 400 - margin.top - margin.bottom,
+  width = 600 - margin.left - margin.right,
   barWidth = 50,
   barOffset = 5;
 
@@ -13,51 +15,85 @@ const yScale = d3
   .domain([0, d3.max(barData)])
   .range([0, height]);
 
+const yAxisValues = d3
+  .scaleLinear()
+  .domain([0, d3.max(barData)])
+  .range([height, 0]);
+
+const yAxisTicks = d3.axisLeft(yAxisValues).ticks(10);
+
 const xScale = d3
   .scaleBand()
   .domain(barData)
-  .paddingInner(0.2)
+  .paddingInner(0.1)
   .paddingOuter(0.1)
   .range([0, width]);
 
-// const colors = d3
-//   .scaleLinear()
-//   .domain([0, d3.max(barData)])
-//   .range(["#FFB832", "#C61C6F"]);
 const colors = d3
   .scaleLinear()
-  .domain([0, barData.length * 0.33, barData.length * 0.66, barData.length])
-  .range(["#FFB832", "#C61C6F", "#268BD2", "#85992C"]);
+  .domain([0, 65, d3.max(barData)])
+  .range(["#FFFFFF", "#268BD2", "#DA3637"]);
 
 export function buildSimpleBar(_data, container_id) {
-  d3.select(container_id)
+  const tooltip = d3
+    .select("body")
+    .append("div")
+    .style("position", "absolute")
+    .style("padding", "0 10px")
+    .style("background", "white")
+    .style("opacity", 0);
+
+  const chart = d3
+    .select(container_id)
     .append("svg")
+    //.style("background", "#dff0d8")
     .attr("width", width)
     .attr("height", height)
-    .style("background", "#dff0d8")
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.right + ")")
     .selectAll("rect")
     .data(barData)
     .enter()
     .append("rect")
-    .style("fill", function (d) {
-      return colors(d);
-    })
+    .style("fill", colors)
     .attr("width", function (d) {
       return xScale.bandwidth();
     })
-    .attr("height", function (d) {
-      return yScale(d);
-    })
+    .attr("height", 0)
     .attr("x", function (d) {
       return xScale(d);
     })
-    .attr("y", function (d) {
-      return height - yScale(d);
-    })
+    .attr("y", height)
     .on("mouseover", function (d) {
+      // tooltip.transition().duration(200).style("opacity", 0.9);
+      // tooltip
+      //   .html(d)
+      //   .style("left", d3.event.pageX - 35 + "px")
+      //   .style("top", d3.event.pageY - 30 + "px");
+
       d3.select(this).style("opacity", 0.5);
     })
     .on("mouseout", function (d) {
       d3.select(this).style("opacity", 1);
     });
+
+  const yGuide = d3
+    .select(container_id + " svg")
+    .attr("transform", "translate(20,0)")
+    .append("g")
+    .call(yAxisTicks);
+
+  chart
+    .transition()
+    .attr("height", function (d) {
+      return yScale(d);
+    })
+    .attr("y", function (d) {
+      return height - yScale(d);
+    })
+    .delay(function (d, i) {
+      return i * 20;
+    })
+    .duration(300)
+    .ease(d3.easeBounceOut);
 }
